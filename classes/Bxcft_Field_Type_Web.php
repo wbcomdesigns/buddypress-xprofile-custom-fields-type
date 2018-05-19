c0d3c6d7effc9f4847853f8fc79756ade75b11cb
Varun Dubey <vapvarun@gmail.com>
2018-05-19 12:13:38 +0530

    WPCS fixes

--- classes/Bxcft_Field_Type_Web.php
+++ classes/Bxcft_Field_Type_Web.php
@@ -2,95 +2,101 @@
 /**
  * Web Type
  */
-if (!class_exists('Bxcft_Field_Type_Web'))
-{
-    class Bxcft_Field_Type_Web extends BP_XProfile_Field_Type
-    {
-        public function __construct() {
-            parent::__construct();
+if ( ! class_exists( 'Bxcft_Field_Type_Web' ) ) {
+	class Bxcft_Field_Type_Web extends BP_XProfile_Field_Type {
 
-            $this->name             = _x( 'Website (HTML5 field)', 'xprofile field type', 'bxcft' );
+		public function __construct() {
+			parent::__construct();
 
-            $this->set_format( '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS', 'replace' );
-            do_action( 'bp_xprofile_field_type_web', $this );
-        }
+			$this->name = _x( 'Website (HTML5 field)', 'xprofile field type', 'bxcft' );
 
-        public function admin_field_html (array $raw_properties = array ())
-        {
-            $html = $this->get_edit_field_html_elements( array_merge(
-                array( 'type' => 'url' ),
-                $raw_properties
-            ) );
-        ?>
-            <input <?php echo $html; ?> />
-        <?php
-        }
+			$this->set_format( '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS', 'replace' );
+			do_action( 'bp_xprofile_field_type_web', $this );
+		}
 
-        public function edit_field_html (array $raw_properties = array ())
-        {
-            if ( isset( $raw_properties['user_id'] ) ) {
-                unset( $raw_properties['user_id'] );
-            }
+		public function admin_field_html( array $raw_properties = array() ) {
+			$html = $this->get_edit_field_html_elements(
+				array_merge(
+					array( 'type' => 'url' ),
+					$raw_properties
+				)
+			);
+		?>
+			<input <?php echo $html; ?> />
+		<?php
+		}
 
-            // HTML5 required attribute.
-            if ( bp_get_the_profile_field_is_required() ) {
-                $raw_properties['required'] = 'required';
-            }
+		public function edit_field_html( array $raw_properties = array() ) {
+			if ( isset( $raw_properties['user_id'] ) ) {
+				unset( $raw_properties['user_id'] );
+			}
 
-            $html = $this->get_edit_field_html_elements( array_merge(
-                array(
-                    'type'  => 'url',
-                    'value' => bp_get_the_profile_field_edit_value(),
-                ),
-                $raw_properties
-            ) );
+			// HTML5 required attribute.
+			if ( bp_get_the_profile_field_is_required() ) {
+				$raw_properties['required'] = 'required';
+			}
 
-            $label = sprintf(
-                '<label for="%s">%s%s</label>',
-                    bp_get_the_profile_field_input_name(),
-                    bp_get_the_profile_field_name(),
-                    (bp_get_the_profile_field_is_required()) ?
-                        ' ' . esc_html__( '(required)', 'buddypress' ) : ''
-            );
-            // Label.
-            echo apply_filters('bxcft_field_label', $label, bp_get_the_profile_field_id(), bp_get_the_profile_field_type(), bp_get_the_profile_field_input_name(), bp_get_the_profile_field_name(), bp_get_the_profile_field_is_required());
-            // Errors.
-            do_action( bp_get_the_profile_field_errors_action() );
-            // Input.
-        ?>
-            <input <?php echo $html; ?> />
-        <?php
-        }
+			$html = $this->get_edit_field_html_elements(
+				array_merge(
+					array(
+						'type'  => 'url',
+						'value' => bp_get_the_profile_field_edit_value(),
+					),
+					$raw_properties
+				)
+			);
 
-        public function admin_new_field_html( BP_XProfile_Field $current_field, $control_type = '' ) {}
+			$label = sprintf(
+				'<label for="%s">%s%s</label>',
+				bp_get_the_profile_field_input_name(),
+				bp_get_the_profile_field_name(),
+				( bp_get_the_profile_field_is_required() ) ?
+						' ' . esc_html__( '(required)', 'buddypress' ) : ''
+			);
+			// Label.
+			echo apply_filters( 'bxcft_field_label', $label, bp_get_the_profile_field_id(), bp_get_the_profile_field_type(), bp_get_the_profile_field_input_name(), bp_get_the_profile_field_name(), bp_get_the_profile_field_is_required() );
+			// Errors.
+			do_action( bp_get_the_profile_field_errors_action() );
+			// Input.
+		?>
+			<input <?php echo $html; ?> />
+		<?php
+		}
 
-        /**
-         * Modify the appearance of value. No autolink feature for Web type.
-         *
-         * @param  string   $value      Original value of field
-         * @param  int      $field_id   Id of field
-         * @return string   Value formatted
-         */
-        public static function display_filter($field_value, $field_id = '') {
+		public function admin_new_field_html( BP_XProfile_Field $current_field, $control_type = '' ) {}
 
-            $new_field_value = $field_value;
+		/**
+		 * Modify the appearance of value. No autolink feature for Web type.
+		 *
+		 * @param  string $value      Original value of field
+		 * @param  int    $field_id   Id of field
+		 * @return string   Value formatted
+		 */
+		public static function display_filter( $field_value, $field_id = '' ) {
 
-            if (!empty($field_value)) {
-                $new_field_value = sprintf('<a href="%1$s">%1$s</a>',
-                    $field_value);
-            }
+			$new_field_value = $field_value;
 
-            /**
-             * bxcft_web_display_filter
-             *
-             * Use this filter to modify the appearance of Web
-             * field value.
-             * @param  $new_field_value Value of field
-             * @param  $field_id Id of field.
-             * @return  Filtered value of field.
-             */
-            return apply_filters('bxcft_web_display_filter',
-                $new_field_value, $field_id);
-        }
-    }
+			if ( ! empty( $field_value ) ) {
+				$new_field_value = sprintf(
+					'<a href="%1$s">%1$s</a>',
+					$field_value
+				);
+			}
+
+			/**
+			 * bxcft_web_display_filter
+			 *
+			 * Use this filter to modify the appearance of Web
+			 * field value.
+			 *
+			 * @param  $new_field_value Value of field
+			 * @param  $field_id Id of field.
+			 * @return  Filtered value of field.
+			 */
+			return apply_filters(
+				'bxcft_web_display_filter',
+				$new_field_value, $field_id
+			);
+		}
+	}
 }
